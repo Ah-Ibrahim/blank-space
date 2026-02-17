63809bd83ad7700de2aa97379f0b6f9af5eefd22
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-17 21:54:36 +0200

    feat: add mode toggle in navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,15 +1,19 @@
 import Logo from "@/components/ui/logo";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 import NavbarButtons from "./navbar-buttons";
 
 function Navbar() {
   return (
     <header>
       <nav className="px-8 py-4 flex justify-between">
-        <div className="flex items-center gap-x-2">
+        <div className="flex items-center gap-x-2 ">
           <Logo className="w-8 h-8" />
           BlankSpace
         </div>
-        <NavbarButtons />
+        <div className="flex items-center gap-x-4">
+          <NavbarButtons />
+          <ModeToggle />
+        </div>
       </nav>
     </header>
   );
