613ea3a811fb724a181c3639d401b407aadcf279
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-17 21:50:18 +0200

    fix: check if window object is defined for client rendering in useMediaQuery hook

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -3,10 +3,18 @@
 import { useEffect, useState } from "react";
 
 function useMediaQuery(query: string) {
-  const [matches, setMatches] = useState<boolean>(false);
+  const getMatches = () => {
+    if (typeof window === "undefined") {
+      return false;
+    }
+    return window.matchMedia(query).matches;
+  };
+
+  const [matches, setMatches] = useState<boolean>(getMatches());
 
   useEffect(() => {
     const mediaQuery = window.matchMedia(query);
+
     const handleChange = (e: MediaQueryListEvent) => setMatches(e.matches);
     mediaQuery.addEventListener("change", handleChange);
 
