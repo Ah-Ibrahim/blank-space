e72d648ad059275264ca2a09a1dcfbbb20891665
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-17 13:36:51 +0200

    feat: add heading

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,11 @@
+function Heading() {
+  return (
+    <div>
+      <h1 className="text-3xl md:text-4xl lg:text-5xl text-center font-bold">
+        Your Ideas💡, Documents📕, & Plans🚀. Welcome to{" "}
+        <span className="underline">BlankSpace</span>!
+      </h1>
+    </div>
+  );
+}
+export default Heading;
