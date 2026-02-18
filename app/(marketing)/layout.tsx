b58297ddca71938e7a7b0b598ed2ed5e3bb8d464
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-18 22:53:49 +0200

    fix: change height of landing page to 100% instead of screen

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "./_components/navbar";
 
 function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen dark:bg-[#1f1f1f]">
+    <div className="h-full dark:bg-[#1f1f1f]">
       <Navbar />
       <main>{children}</main>
     </div>
