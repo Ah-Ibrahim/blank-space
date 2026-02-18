990b14b29e0dfbd7e0f29069c8a30590bd745ab6
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-18 18:52:38 +0200

    feat: update spinner to have multiple variants

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,6 +1,27 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
 import { LoaderCircle } from "lucide-react";
 
-function Spinner() {
-  return <LoaderCircle className="animate-spin" />;
+const spinnerVariants = cva(
+  "text-muted-foreground animate-spin aspect-square",
+  {
+    variants: {
+      size: {
+        default: "w-4",
+        sm: "w-2",
+        lg: "w-6",
+        icon: "w-10",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  },
+);
+
+type SpinnerProps = VariantProps<typeof spinnerVariants>;
+
+function Spinner({ size }: SpinnerProps) {
+  return <LoaderCircle className={cn(spinnerVariants({ size }))} />;
 }
 export default Spinner;
