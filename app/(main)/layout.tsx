7c9fc1dc40330a3d5ae83da41488e4888fc276b8
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-18 23:18:04 +0200

    feat: add MainLayout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
+
+async function MainLayout({ children }: { children: React.ReactNode }) {
+  const { userId } = await auth();
+
+  if (!userId) redirect("/");
+
+  return (
+    <div className="h-full dark:bg-[#1f1f1f] flex">
+      <Navigation />
+      <main className="h-full flex-1 overflow-y-auto">{children}</main>
+    </div>
+  );
+}
+export default MainLayout;
