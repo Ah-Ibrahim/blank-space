eaf81bfb0d4eade6c3c70e64ef6bd35d25c3b623
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-18 18:54:40 +0200

    fix: depend on css to hide hero button instead of useMediaQuery for hydration error

--- app/(marketing)/_components/navbar-buttons.tsx
+++ app/(marketing)/_components/navbar-buttons.tsx
@@ -2,24 +2,21 @@
 
 import { Button } from "@/components/ui/button";
 import Spinner from "@/components/ui/spinner";
-import useMediaQuery from "@/hooks/useMediaQuery";
-import screenBreakpoints from "@/lib/breakpoints";
 import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
 import { Authenticated, AuthLoading, Unauthenticated } from "convex/react";
 import { ArrowRightIcon } from "lucide-react";
 
 function NavbarButtons() {
-  const isMdOrLarger = useMediaQuery(`(min-width: ${screenBreakpoints.md})`);
-
   return (
     <>
       <Unauthenticated>
         <div className="flex gap-x-4">
-          {isMdOrLarger && (
-            <SignInButton mode="modal">
-              <Button variant={"ghost"}>Log in</Button>
-            </SignInButton>
-          )}
+          <SignInButton mode="modal">
+            <Button variant={"ghost"} className="hidden md:block">
+              Log in
+            </Button>
+          </SignInButton>
+
           <SignUpButton mode="modal">
             <Button>
               Get BlankSpace <ArrowRightIcon />
