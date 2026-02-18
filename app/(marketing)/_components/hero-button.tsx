6ae7c1e892215808cddd4ca257a33330d0b3a2f2
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-18 18:56:21 +0200

    feat: add documents link to landing page

--- app/(marketing)/_components/hero-button.tsx
+++ app/(marketing)/_components/hero-button.tsx
@@ -5,12 +5,17 @@ import Spinner from "@/components/ui/spinner";
 import { SignUpButton } from "@clerk/clerk-react";
 import { Authenticated, AuthLoading, Unauthenticated } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 function HeroButton() {
   return (
     <>
       <Authenticated>
-        <Button>Enter BlankSpace</Button>
+        <Button asChild>
+          <Link href="/documents">
+            Enter BlankSpace <ArrowRight />
+          </Link>
+        </Button>
       </Authenticated>
       <Unauthenticated>
         <SignUpButton mode="modal">
