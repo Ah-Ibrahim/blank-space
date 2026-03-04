c0e51a41defd6d557c54317ed631bb2afea4ad3a
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-03-04 22:59:43 +0200

    feat: add modal provider and search command to main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,8 @@
+import ModalProvider from "@/providers/modal-provider";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navigation from "./_components/navigation";
+import SearchCommand from "./_components/search-command";
 
 async function MainLayout({ children }: { children: React.ReactNode }) {
   const { userId } = await auth();
@@ -8,10 +10,13 @@ async function MainLayout({ children }: { children: React.ReactNode }) {
   if (!userId) redirect("/");
 
   return (
-    <div className="h-full flex">
-      <Navigation />
-      <main className="h-full flex-1 overflow-y-auto">{children}</main>
-    </div>
+    <ModalProvider>
+      <div className="h-full flex">
+        <Navigation />
+        <SearchCommand />
+        <main className="h-full flex-1 overflow-y-auto">{children}</main>
+      </div>
+    </ModalProvider>
   );
 }
 export default MainLayout;
