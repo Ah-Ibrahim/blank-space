1dce286e92ebd04407aeeae2d5aa684c3cbdc4c8
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-03-04 22:27:50 +0200

    feat: add inter font as base font

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,16 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <script
-          async
-          crossOrigin="anonymous"
-          src="https://tweakcn.com/live-preview.min.js"
-        />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
         <ClerkProvider dynamic>
           <ConvexClientProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
