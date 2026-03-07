445e0f8b22fc1c3074900a07ad80d22078450a92
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-03-08 00:30:35 +0200

    feat: add document title component

---
+++ app/(main)/_components/document-title.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { useMutation } from "convex/react";
+import { useRef, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+
+interface DocumentTitleProps {
+  initialData: Doc<"documents">;
+  isPreview: boolean;
+}
+
+function DocumentTitle({ initialData, isPreview }: DocumentTitleProps) {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [title, setTitle] = useState<string>(initialData.title);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const update = useMutation(api.documents.update);
+
+  const enableInput = () => {
+    if (isPreview) return;
+
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+      textareaRef.current?.setSelectionRange(title.length, title.length);
+    });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTitle(e.target.value);
+
+    update({
+      id: initialData._id,
+      title: e.target.value.trim() || "Untitled",
+    });
+  };
+
+  const disableInput = () => {
+    setIsEditing(false);
+    setTitle(initialData.title);
+  };
+
+  return (
+    <>
+      {isEditing && !isPreview ? (
+        <TextareaAutosize
+          ref={textareaRef}
+          value={title}
+          onChange={handleChange}
+          className={cn(
+            "w-full resize-none overflow-hidden bg-transparent text-[#3f3f3f] dark:text-[#cfcfcf] font-bold focus:outline-none text-5xl cursor-pointer focus:cursor-text",
+          )}
+          spellCheck={false}
+          onBlur={disableInput}
+        />
+      ) : (
+        <div
+          className={cn(
+            "w-full overflow-hidden break-words whitespace-pre-wrap bg-transparent text-[#3f3f3f] dark:text-[#cfcfcf] font-bold focus:outline-none text-5xl cursor-pointer focus:cursor-text",
+            isPreview && "cursor-default",
+          )}
+          onClick={enableInput}
+        >
+          {initialData.title}
+        </div>
+      )}
+    </>
+  );
+}
+export default DocumentTitle;
