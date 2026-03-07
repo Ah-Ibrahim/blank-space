d9ced31c90f86bc2441ef7a19d95d4ef1f4b09cb
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-03-08 00:31:31 +0200

    feat: add remove icon server function

--- convex/documents.ts
+++ convex/documents.ts
@@ -277,3 +277,25 @@ export const update = mutation({
     await ctx.db.patch("documents", id, rest);
   },
 });
+
+export const removeIcon = mutation({
+  args: {
+    id: v.id("documents"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) throw new Error("Unauthenticated call to mutation");
+
+    const userId = identity.subject;
+    const { id } = args;
+
+    const document = await ctx.db.get(id);
+
+    if (document === null) throw new Error("Document not found");
+    if (document.userId !== userId)
+      throw new Error("Unauthorized access to document");
+
+    await ctx.db.patch("documents", id, { icon: undefined });
+  },
+});
