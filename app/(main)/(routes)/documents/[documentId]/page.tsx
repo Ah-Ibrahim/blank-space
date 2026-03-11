72978f595d558007a8c8f604639ec6f5d91f45e7
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-03-11 04:49:55 +0200

    feat: add cover and toolbar to document page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,10 +1,28 @@
-async function DocumentPage({
-  params,
-}: {
-  params: Promise<{ documentId: string }>;
-}) {
-  const { documentId } = await params;
+"use client";
 
-  return <div>{documentId}</div>;
+import Cover from "@/components/cover";
+import Toolbar from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+import Loading from "./loading";
+
+function DocumentPage() {
+  const { documentId } = useParams();
+  const document = useQuery(api.documents.getById, {
+    documentId: documentId as Id<"documents">,
+  });
+
+  if (document === undefined) return <Loading />;
+
+  return (
+    <div className="pb-40">
+      <Cover url={document.coverImg} />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 }
 export default DocumentPage;
