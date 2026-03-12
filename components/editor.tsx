8bcebef1cb6347cee5fcc320188ac8095f558517
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-03-13 00:01:32 +0200

    feat: add theme support for editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -5,14 +5,21 @@ import "@blocknote/core/fonts/inter.css";
 import { useCreateBlockNote, useEditorChange } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/shadcn";
 import "@blocknote/shadcn/style.css";
+import { useTheme } from "next-themes";
 
 interface EditorProps {
+  // This is recommended to way from docs
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   initialContent?: Block<any, any, any>[];
   editable: boolean;
   onChange: (value: string) => void;
 }
 
 function Editor({ initialContent, editable, onChange }: EditorProps) {
+  const { resolvedTheme } = useTheme();
+
+  const theme = resolvedTheme === "dark" ? "dark" : "light";
+
   const editor = useCreateBlockNote({
     initialContent,
   });
@@ -23,6 +30,6 @@ function Editor({ initialContent, editable, onChange }: EditorProps) {
     onChange(savedBlocks);
   }, editor);
 
-  return <BlockNoteView editor={editor} />;
+  return <BlockNoteView editor={editor} editable={editable} theme={theme} />;
 }
 export default Editor;
