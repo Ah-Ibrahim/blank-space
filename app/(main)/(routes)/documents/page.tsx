c4a47e017bcba2c72518b2f5b991a8091282c477
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-03-13 02:07:02 +0200

    fix: add missing routing on creating page from main screen

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -6,14 +6,18 @@ import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 function DocumentsPage() {
   const { user } = useUser();
   const create = useMutation(api.documents.create);
+  const router = useRouter();
 
   const onCreate = () => {
-    const promise = create({ title: "Untitled" });
+    const promise = create({ title: "Untitled" }).then((id) => {
+      router.push(`/documents/${id}`);
+    });
 
     toast.promise(promise, {
       loading: "Creating Note",
