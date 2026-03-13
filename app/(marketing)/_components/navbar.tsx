10be6f078f9b6306dc7d8622b7f332f76e509fd8
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-03-13 05:34:10 +0200

    feat: change brand's logo

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,14 +1,20 @@
-import Logo from "@/components/ui/logo";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import Image from "next/image";
 import NavbarButtons from "./navbar-buttons";
 
 function Navbar() {
   return (
     <header>
       <nav className="px-8 py-4 flex justify-between">
-        <div className="flex items-center gap-x-2 ">
-          <Logo className="w-8 h-8" />
-          BlankSpace
+        <div className="flex items-center gap-x-2 text-xl">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={217}
+            height={217}
+            className="invert w-10 aspect-auto dark:invert-0"
+          />
+          Blankspace
         </div>
         <div className="flex items-center gap-x-4">
           <NavbarButtons />
